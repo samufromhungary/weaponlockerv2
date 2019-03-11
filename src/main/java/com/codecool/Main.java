e89708647a3b81f91a19a18243edf5629ef9ad5f
Martin Cserép <martincserep1@gmail.com>
2019-03-11 09:41:20 +0100

    First Commit

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -2,12 +2,6 @@ package com.codecool;
 
 public class Main {
 
-    public String getWelcomeString() {
-        return "Hi!";
-    }
-
     public static void main(String[] args) {
-        Main main = new Main();
-        System.out.println(main.getWelcomeString());
     }
 }
