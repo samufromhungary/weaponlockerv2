3ece975a1f4143dbbf77838c7bb7f0ba9bd52008
Martin Cserép <martincserep1@gmail.com>
2019-03-11 10:59:39 +0100

    created the serer and client package and the main classes

---
+++ src/main/java/com/codecool/client/Client.java
@@ -0,0 +1,7 @@
+package com.codecool.client;
+
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/codecool/server/Server.java
@@ -0,0 +1,7 @@
+package com.codecool.server;
+
+public class Server {
+    public static void main(String[] args) {
+
+    }
+}
