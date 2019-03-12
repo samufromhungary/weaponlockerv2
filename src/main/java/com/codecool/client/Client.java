86e35baef0e7924eab4805744a4e08c7ba6c420f
samufromhungary <blzkproblem@gmail.com>
2019-03-12 13:40:40 +0100

    initial

--- src/main/java/com/codecool/client/Client.java
+++ src/main/java/com/codecool/client/Client.java
@@ -4,6 +4,9 @@ import com.codecool.common.Measurement;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Client {
@@ -13,23 +16,51 @@ public class Client {
         run(args);
     }
 
+    public Random rand = new Random();
+    Scanner sc = new Scanner(System.in);
+    List<Measurement> ls = new ArrayList<>();
+
+    public List<Measurement> generator() {
+        System.out.println("Choose an id");
+        int id = Integer.valueOf(sc.nextLine());
+        System.out.println("Choose how many measurements you want to send");
+        int n = Integer.valueOf(sc.nextLine());
+        for (int i = 0; i < n ; i++) {
+            ls.add(new Measurement(id, rand.nextInt(500), "mm"));
+        }
+        return ls;
+    }
+
     private void run(String[] args) throws  IOException {
         String host = args[1];
         int port = Integer.parseInt(args[2]);
         Socket clientSocket = new Socket(host, port);
-        Scanner sc = new Scanner(System.in);
+        System.out.println("Choose an id");
         int id = Integer.valueOf(sc.nextLine());
-        int value = Integer.valueOf(sc.nextLine());
-        String type = sc.nextLine();
-        Measurement m = new Measurement(id, value, type);
+        System.out.println("Choose how many measurements you want to send");
+        int n = Integer.valueOf(sc.nextLine());
         try{
             ObjectOutputStream os = new ObjectOutputStream(clientSocket.getOutputStream());
             ObjectInputStream is = new ObjectInputStream(clientSocket.getInputStream());
-            os.writeObject(m);
-            Measurement rm = (Measurement) is.readObject();
+            for (int i = 0; i < n ; i++) {
+                os.writeObject(new Measurement(id, rand.nextInt(500), "mm"));
+                Measurement m = (Measurement) is.readObject();
+            }
+
+            //            os.writeObject();
+//            Measurement rm = (Measurement) is.readObject();
             clientSocket.close();
         }catch (IOException | ClassNotFoundException e){
             System.out.println(e.getMessage());
         }
     }
 }
+
+
+
+
+/*
+for (int i = 0; i < n ; i++){
+
+}
+ */
--- src/main/java/com/codecool/server/Server.java
+++ src/main/java/com/codecool/server/Server.java
@@ -12,27 +12,33 @@ import java.net.Socket;
 public class Server {
 
     public Server(String[] args) throws IOException {
-        run(args[1]);
+        runServer(args[1]);
     }
 
-    private void run(String arg) throws  IOException{
-        int port = Integer.parseInt(arg);
-        ServerSocket ss = new ServerSocket(port);
-        System.out.println("Server is very STABIL");
-        Socket socket = ss.accept();
-        ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
-        ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
-        XMLHandler xml = new XMLHandler();
-        try {
-            Measurement m = (Measurement) is.readObject();
-            xml.handle(m);
-            os.writeObject(m);
-            socket.close();
-        } catch (IOException | ClassNotFoundException e ) {
-            e.printStackTrace();
+    private void runServer(String arg) throws IOException {
+        int portNumber = Integer.parseInt(arg);
+        ServerSocket ss = new ServerSocket(portNumber);
+        System.out.println("Server online\n\n");
+        while (true) {
+            final Socket socket = ss.accept();
+            new Thread(() -> {
+                try {
+                    ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
+                    ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
+                    XMLHandler xml = new XMLHandler();
+                    Measurement m = (Measurement) is.readObject();
+                    xml.handle(m);
+                    os.writeObject(m);
+                    System.out.println("Server is closed");
+                    socket.close();
+
+                } catch (IOException | ClassNotFoundException e ) {
+                    e.printStackTrace();
+                }
+            }).start();
+
         }
     }
-
 
 }
 
